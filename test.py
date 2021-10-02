a59301c133af06aaf6b13591b41bf3711622b879
dontseemenot <artsze10@gmail.com>
2021-10-02 17:40:46 +1000

    Initial commit for database branch

---
+++ test.py
@@ -0,0 +1,8 @@
+# %%
+
+# %%
+# Embed or not?
+# do you need the embedded data 80% of the time
+# How often you want the embedded data without document?
+# is embedded data bounded?
+# ratings should be ok as an unboudned set to embed
