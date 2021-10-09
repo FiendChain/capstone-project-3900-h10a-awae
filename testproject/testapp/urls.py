8ced3a3f1f09442f9064cf5f11d52b513d9af932
William Yang <williamyang2017@gmail.com>
2021-10-09 16:07:17 +1100

    Added views and url endpoints for pages

--- testproject/testapp/urls.py
+++ testproject/testapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from.import views
+from . import views
 
 urlpatterns = [
-    path('message/', views.displayMessage)
+    path('message/', views.displayMessage),
+    path('home/', views.homepage, name="homepage"),
+    path('login/', views.login, name="login"),
+    path('registration/', views.registration, name="registration"),
+
 ]
--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def displayMessage(request):
     return HttpResponse('What does the fox say?')
+
+def homepage(request):
+    return render(request, "homepage.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def registration(request):
+    return render(request, "registration.html")
