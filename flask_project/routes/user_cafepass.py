3c9a84d3cbe6d69e93f027eda26d8bddd1107a0e
dontseemenot <artsze10@gmail.com>
2021-11-03 22:43:26 +1100

    cafepass and upgrade function

--- flask_project/routes/user_cafepass.py
+++ flask_project/routes/user_cafepass.py
@@ -8,11 +8,12 @@ from flask_login.utils import login_required
 
 from server import app, get_db
 from .endpoints import user_bp, api_bp
-from .forms import CafePassForm, serialize_form
+from .forms import CafePassForm, serialize_form, PaymentCardForm, valid_states
 
 import datetime
 
 from classes.cafepass import get_cafepass, CafepassInfo
+from classes.profile_payment import get_default_billing_info, get_default_payment_info
 
 @app.before_request
 def create_cafepass():
@@ -35,14 +36,32 @@ def profile_membership():
     user_id = current_user.get_id()
     cafepass = get_cafepass(user_id)
 
-    form = CafePassForm()
-    form.paid.data = bool(cafepass['paid'])
+    cafepass_form = CafePassForm()
+    form = PaymentCardForm()
+    cafepass_form.paid.data = bool(cafepass['paid'])
 
-    return render_template("profile/membership.html", form=form)
+    default_billing_info = get_default_billing_info()
+    info = get_default_payment_info()
+    default_payment_info = info and dict(
+        cc_name=info["name"],
+        cc_number=info["number"],
+        cc_expiry=info["expiry"],
+        cc_cvc=info["cvc"] 
+    )
+
+    data = dict(
+        form=form,  # payment and billing form
+        cafepass_form=cafepass_form,
+        default_billing_info=default_billing_info,
+        default_payment_info=default_payment_info,
+        valid_states=valid_states
+    )
+    return render_template("profile/membership.html", **data)
 
 @api_bp.route('/profile/membership', methods=["POST"])
 @login_required
 def profile_membership():
+    print("BATTLEP[ASS BOUGHT")
     form = CafePassForm()
     if not form.validate_on_submit():
         return redirect(url_for("user_bp.profile_membership"))
@@ -50,7 +69,8 @@ def profile_membership():
     user_id = current_user.get_id()
     cafepass = get_cafepass(user_id)
     cafepass_old = list(cafepass.values())
-    cafepass['paid'] = int(form.paid.data)
+    # cafepass['paid'] = int(form.paid.data)
+    cafepass['paid'] = 1
     cafepass_new = list(cafepass.values())
 
     with app.app_context():
