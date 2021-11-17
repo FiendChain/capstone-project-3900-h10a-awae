f51271810a32d6cd274c0606a7e3a5263f9457f1
William Yang <williamyang2017@gmail.com>
2021-11-17 22:59:20 +1100

    Updated for demo setup

--- flask_project/run.py
+++ flask_project/run.py
@@ -1,19 +1,19 @@
-from livereload import Server
 from server import app, login_manager
 
 import routes
 from routes import user_bp, admin_bp, api_bp, admin_api_bp
 
-from flask_bootstrap import Bootstrap
-
 import argparse 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--no-livereload", action="store_true")
+    parser.add_argument("--no-livereload", action="store_true", help="Disable live reload when changes are made to server")
+    parser.add_argument("--host", default="127.0.0.1", type=str, help="IP address of server")
+    parser.add_argument("--port", default=5002, type=int, help="Port of server")
+    parser.add_argument("--debug", action="store_true", help="Enable server debugging output")
     args = parser.parse_args()
 
-    app.jinja_env.auto_reload = True
+    app.jinja_env.auto_reload = not args.no_livereload
 
     # add blueprint for handling user and admin endpoints
     app.register_blueprint(user_bp, url_prefix='/')
@@ -21,15 +21,14 @@ if __name__ == "__main__":
     app.register_blueprint(api_bp, url_prefix='/api_v1')
     app.register_blueprint(admin_api_bp, url_prefix='/admin/api_v1')
 
-    flask_bs = Bootstrap(app)
-
     # handle login
     login_manager.init_app(app)
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.debug = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = not args.no_livereload
+    app.debug = args.debug
 
     if args.no_livereload:
-        app.run(port=5002, debug=True)
+        app.run(host=args.host, port=args.port, debug=args.debug)
     else:
+        from livereload import Server
         server = Server(app.wsgi_app)
-        server.serve(port=5002, debug=True, liveport=5002)
+        server.serve(host=args.host, port=args.port, debug=args.debug, liveport=args.port)
